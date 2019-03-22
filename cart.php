7c79082658e869ab8f503e14ceeef2495e686877
SS/ShadowsocksR Server <45452928+ss-ssr@users.noreply.github.com>
2019-03-22 15:25:43 +0800

    Add files via upload

---
+++ cart.php
@@ -0,0 +1,109 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8" />
+	<meta name=”baiduspider” content=”nofollow”>
+    <meta name=”baiduspider” content=”noarchive”>
+	<meta name="keywords" content="Shadowsocks,ShadowsocksX,ShadowsocksX-NG,Shadowrocket,Wingy,VPN,ShadowVPN" />
+	<meta name="description" content="Shadowsocks - A secure socks5 proxy" />
+	<title>Shadowsocks - A secure socks5 proxy</title>
+	<link href="/favicon.png" rel="shortcut icon">
+	<!--<link rel="stylesheet" href="css/style.css" type="text/css">-->
+	<link rel="stylesheet" href="css/app.css" type="text/css">
+	
+</head>
+<body>
+	<div id="wrap" class="boxed">
+<header>
+            <div class="container clearfix">
+                    <div class="four columns"><div class="logo"><a href="index.html" style="font-size:28px;">Shadowsocks.Network</a></div></div>
+                    <div class="twelve columns">
+                        <nav id="menu" class="navigation">
+                            <ul id="nav">
+                                <li style="z-index: 100;">
+                                    <a href="https://cp.shadowsocks.network" class="">首页</a>
+                                </li>
+                                <li style="z-index: 99;"><a href="https://shadowsocks.network" class="">下载</a></li>
+                                <li style="z-index: 98;"><a href="https://shadowsocks.network" class="">帮助</a></li>
+                                <li style="z-index: 97;"><a href="https://shadowsocks.network" class="">联系</a></li>
+                                <li style="z-index: 95;"><a href="https://shadowsocks.network" class="">登陆</a></li>
+                            </ul>
+                        </nav>
+                    </div>
+                    <div class="sixteen columns"><hr>
+                    </div>
+            </div>
+</header>		<div class="container clearfix">
+            <div id="welcome" class="sixteen columns">
+                <div class="qoute">
+					<div class="ten-and-half columns omega"><h1>一个安全的Socks5代理，</h1><h1>旨在保护您的Internet流量。</h1></div>
+					<div class="four-and-half columns alpha">
+						
+						<a href="https://shadowsocks.network" class="btn-download link"><span><i class="fa fa-download fa-lg"></i>&nbsp; 现在就试试！</span></a>
+						<a href="https://shadowsocks.network" class="btn-support link"><span><i class="fa fa-phone fa-lg"></i>&nbsp; 需要支持</span></a>
+					</div>
+				</div>
+			</div>
+		</div>
+		 
+		<div class="container clearfix">
+			<div id="brand" class="sixteen columns bottoms">
+				<blockquote class="large center">如果我们是鸟人，我们可以到达世界的每个角落。</blockquote>
+			</div>
+		</div>
+                 
+
+		<div class="container clearfix">
+			<div class="our-services">
+				<ul>
+					<li class="one-third column">
+						<div class="icon-box"><i class="fa fa-bolt fa-2x"></i></div>
+						<h3>超级快</h3>
+						<p>让你体会到惊人的速度。</p>
+					</li>
+
+					<li class="one-third column">
+						<div class="icon-box">
+							<i class="fa fa-qrcode fa-2x"></i>
+						</div>
+						<h3>简单</h3>
+						<p>直接扫码即可使用。</p>
+					</li>
+					<li class="one-third column">
+						<div class="icon-box">
+							<i class="fa fa-shield fa-2x"></i>
+						</div>
+						<h3>安全</h3>
+						<p>使用行业级加密算法保护安全。</p>
+					</li>
+					<li class="one-third column">
+						<div class="icon-box">
+							<i class="fa fa-desktop fa-2x"></i></div>
+							<h3>设备</h3>
+							<p>支持微软、苹果、安卓等所有设备。</p>
+					</li>
+					<li class="one-third column">
+						<div class="icon-box"><i class="fa fa-globe fa-2x"></i></div>
+						<h3>覆盖</h3>
+						<p>节点覆盖面广。</p></li>
+	
+					<li class="one-third column">
+						<div class="icon-box"><i class="fa fa-cloud-download fa-2x"></i></div>
+						<h3>稳定</h3>
+						<p>專業開發運維經驗。</p>
+					</li>
+				</ul>
+			</div>
+		</div>		
+<footer>
+	<div class="container">
+		<div class="sisteen columns">
+			<span class="copyright">
+				<a href="https://shadowsocks.network">Shadowsocks.Network</a>
+			</span>
+		</div>
+	</div>
+</footer> 
+	</div>
+</body>
+</html>
